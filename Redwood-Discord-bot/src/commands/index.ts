7db32be9ecfc97a98fe9ef0a2cffcf72d4242341
Your Name <huzaifakhurram523@gmail.com>
2026-07-11 02:18:16 +0500

    feat(bot): /identity create|rotate|view; retire /hire

--- Redwood-Discord-bot/src/commands/index.ts
+++ Redwood-Discord-bot/src/commands/index.ts
@@ -4,7 +4,8 @@ import { rosterCommands } from './roster';
 import { rosterAdminCommands } from './rosterAdmin';
 import { securityCommands } from './security';
 import { speechCommands } from './say';
+import { identityCommands } from './identity';
 
-export const commands: Command[] = [help, ...rosterCommands, ...rosterAdminCommands, ...securityCommands, ...speechCommands];
+export const commands: Command[] = [help, ...rosterCommands, ...rosterAdminCommands, ...securityCommands, ...speechCommands, ...identityCommands];
 
 export const commandMap = new Map(commands.map((c) => [c.data.name, c]));
